17d223a8d56d4cee72494f757113252a9c3f29a3
Gibbous101 <saish.bhanage@gmail.com>
2025-08-18 22:07:14 +0530

    update GameMode

--- isl_app/src/pages/GameMode.js
+++ isl_app/src/pages/GameMode.js
@@ -28,7 +28,7 @@ const GameMode = () => {
   useEffect(() => {
     const interval = setInterval(async () => {
       const response = await fetch(
-        "https://isl-app-backend.onrender.com/predict_current"
+        "http://127.0.0.1:5000/predict_current"
       );
       const data = await response.json();
 
